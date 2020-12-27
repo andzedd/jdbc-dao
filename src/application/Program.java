5e3e2f3e44209cc7e793e6eb26cf2ce9b4602840
Matheus S Souza <ssouza.matheus@outlook.com>
2020-12-27 13:28:31 -0300

    update impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,5 +28,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New ID = " + newSeller.getId());
+
+        System.out.println("\n========== TEST 5: sellerUpdate ==========");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
