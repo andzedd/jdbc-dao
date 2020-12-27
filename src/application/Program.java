df3ba123ebb889b58920961e5ff8ef4bd45588ee
Matheus S Souza <ssouza.matheus@outlook.com>
2020-12-27 13:02:20 -0300

    findByDepartment impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,5 +14,10 @@ public class Program {
         System.out.println("========== TEST 1: sellerFindById ==========");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n========== TEST 2: sellerFindByDepartment ==========");
+        Department dep = new Department(2, "Electronics");
+        List<Seller> sellers = sellerDao.findByDepartment(dep);
+        sellers.forEach(i -> System.out.println(i));
     }
 }
